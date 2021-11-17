cf0e93e994273aff3b86d17b0eb914a2799f3182
rben75 <80887725+rben75@users.noreply.github.com>
2021-11-17 12:03:54 +0100

    New things

---
+++ image_encoder.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class CFG:
+    debug = False
+    image_path = image_path
+    captions_path = captions_path
+    batch_size = 32
+    num_workers = 2
+    head_lr = 1e-3
+    image_encoder_lr = 1e-4
+    text_encoder_lr = 1e-5
+    weight_decay = 1e-3
+    patience = 1
+    factor = 0.8
+    epochs = 4
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model_name = 'resnet50'
+    image_embedding = 2048
+    text_encoder_model = "distilbert-base-uncased"
+    text_embedding = 768
+    text_tokenizer = "distilbert-base-uncased"
+    max_length = 200
+
+    pretrained = True # for both image encoder and text encoder
+    trainable = True # for both image encoder and text encoder
+    temperature = 1.0
+
+    # image size
+    size = 224
+
+    # for projection head; used for both image and text encoders
+    num_projection_layers = 1
+    projection_dim = 256
+    dropout = 0.1
+
+class ImageEncoder(nn.Module):
+    """
+    Encode images to a fixed size vector
+    """
+
+    def __init__(
+        self, model_name=CFG.model_name, pretrained=CFG.pretrained, trainable=CFG.trainable
+    ):
+        super().__init__()
+        self.model = timm.create_model(
+            model_name, pretrained, num_classes=0, global_pool="avg"
+        )
+        self.model = models.efficientnet_b7()
+        for p in self.model.parameters():
+            p.requires_grad = trainable
+
+    def forward(self, x):
+        return self.model(x)
---
+++ results.py
@@ -0,0 +1,5 @@
+find_matches(model,
+             image_embeddings,
+             query="one dog sitting on the grass",
+             image_filenames=valid_df['image'].values,
+             n=9)
