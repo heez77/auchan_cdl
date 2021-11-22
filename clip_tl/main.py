bae0da268265e5c406cfcd2fd101a3a852416017
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2021-11-22 15:17:58 +0100

    .

---
+++ clip_tl/main.py
@@ -0,0 +1,17 @@
+import clip
+import os
+import cv2
+
+
+
+def train():
+
+    return
+
+def main():
+    train_df, valid_df = make_train_valid_dfs()
+
+    return
+
+if __name__ == '__main__':
+    train()
