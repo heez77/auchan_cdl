0b18750fc9815a9f846368802ba9ec51a20d01e5
Purjack <92036992+Purjack@users.noreply.github.com>
2021-11-26 13:38:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import pandas as pd
+from formain import write_csv
+from config import CFG
+
+def main():
+    df = pd.read_csv(CFG.path_dataframe)
+    # df = dict({'image' : 'image.jpg',
+    #            'description : 'courte description'})
+    df_label = pd.read_csv(CFG.path_labels)
+    # df_label = dict({'niv1' : 'Label1, Label2, ...'
+    #                  'niv2' : 'Label3, Label4, ...' })
+    threshold_clip = CFG.threshold_clip
+    threshold_dist = CFG.threshold_dist
+    write_csv(df, df_label, threshold_clip, threshold_dist)
+
+if __name__=='__main__':
+    main()
