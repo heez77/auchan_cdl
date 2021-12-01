40632b57d4a995faa072990953d4796ce28517bf
Purjack <geymaxime@gmail.com>
2021-12-01 18:49:04 +0100

    .

--- clip_accuracy.py
+++ clip_accuracy.py
@@ -5,14 +5,14 @@ df = pd.read_csv("gitignore/df.csv")
 
 # Pour Clip directement au niveau 2
 prediction_clip = df
-prediction_clip = prediction_clip[['image_name_MEDIASTEP','image_name','label']]
+prediction_clip = prediction_clip[['image_name_MEDIASTEP','image_name','label_niv2']]
 labels = [] # couple est une liste des correct_label dans l'ordre
 for image in prediction_clip.image_name:
     index = (prediction_clip[prediction_clip.image_name==image].index.values)[0]
-    # Trouver le folder où est image
+    # Trouver le folder où est image    
     for folder in os.listdir("gitignore/RESULTS DATASET V2/RESULTS DATASET V2"):
-        if image in os.path.join("gitignore/RESULTS DATASET V2/RESULTS DATASET V2", folder):
-            prediction_clip.label[index]=folder
+        if '{}.jpg'.format(image) in os.listdir("gitignore/RESULTS DATASET V2/RESULTS DATASET V2/" + folder):
+            prediction_clip.label_niv2[index]=folder
             print('image n°{} done'.format(index))
 prediction_clip.to_csv("gitignore/prediction_clip.csv")
 
@@ -24,27 +24,104 @@ for image in prediction_clip_step.image_name:
     index = (prediction_clip_step[prediction_clip_step.image_name==image].index.values)[0]
     # Trouver le folder où est image
     for folder in os.listdir("gitignore/RESULTS DATASET V2 STEP/RESULTS DATASET V2 STEP"):
-        if image in os.path.join("gitignore/RESULTS DATASET V2 STEP/RESULTS DATASET V2 STEP", folder):
+        if '{}.jpg'.format(image) in os.listdir("gitignore/RESULTS DATASET V2 STEP/RESULTS DATASET V2 STEP/" + folder):
             prediction_clip_step.label[index]=folder
             print('image n°{} done'.format(index))
 prediction_clip_step.to_csv("gitignore/prediction_clip_step.csv")
 
 # Comparer les deux colonnes label des deux nouveaux dataset avec la colonne label de df
-abs_score_clip = 0
-abs_score_clip_step = 0
+import pandas as pd
+import os
+import numpy as np
 
-correct_labels = df.label
-clip_labels = prediction_clip.label
-clip_step_labels = prediction_clip_step.label
+df = pd.read_csv("gitignore/df.csv")
+df = df.reindex(np.linspace(0,len(df)-1,len(df)).astype(int).tolist())
+df = df[['image_name_MEDIASTEP','image_name', 'description', 'label', 'media_url']]
+for image in df.image_name:
+    index = (df[df.image_name==image].index.values)[0]
+    # Trouver le folder où est image    
+    for folder in os.listdir("gitignore/Correct_labels"):
+        if '{}.jpg'.format(image) in os.listdir("gitignore/Correct_labels/" + folder):
+            df.label[index]=folder
+            print('image n°{} done'.format(index))
+df.to_csv("gitignore/df.csv")
+
+
+
+
+import pandas as pd
+import os
+import numpy as np
+
+df = pd.read_csv("gitignore/df.csv")
+df_label = pd.read_csv("gitignore/df_label.csv")
+for i in range(len(df)):
+    index = (df_label[df_label.niv2_fr==df.label_niv2[i]].index.values)[0]
+    df['label_niv1'][i] = df_label.niv1_fr[index]
+df = df[['image_name_MEDIASTEP','image_name', 'description', 'label_niv1', 'label_niv2', 'media_url']]
+df.to_csv("gitignore/df.csv")
+
+import pandas as pd
+import os
+import numpy as np
+prediction_clip = pd.read_csv("gitignore/prediction_clip.csv")
+df_label = pd.read_csv("gitignore/df_label.csv")
+prediction_clip['label_niv1'] = 0
+for i in range(len(prediction_clip)):
+    index = (df_label[df_label.niv2_fr==prediction_clip.label_niv2[i]].index.values)[0]
+    prediction_clip['label_niv1'][i] = df_label.niv1_fr[index]
+prediction_clip = prediction_clip[['image_name_MEDIASTEP','image_name', 'label_niv1', 'label_niv2']]
+prediction_clip.to_csv("gitignore/prediction_clip.csv")
+
+import pandas as pd
+import os
+import numpy as np
+prediction_clip_step = pd.read_csv("gitignore/prediction_clip_step.csv")
+df_label = pd.read_csv("gitignore/df_label.csv")
+prediction_clip_step['label_niv1'] = 0
+for i in range(len(prediction_clip_step)):
+    index = (df_label[df_label.niv2_fr==prediction_clip_step.label_niv2[i]].index.values)[0]
+    prediction_clip_step['label_niv1'][i] = df_label.niv1_fr[index]
+prediction_clip_step = prediction_clip_step[['image_name_MEDIASTEP','image_name', 'label_niv1', 'label_niv2']]
+prediction_clip_step.to_csv("gitignore/prediction_clip_step.csv")
+
+import pandas as pd
+import os
+import numpy as np
+
+df = pd.read_csv("gitignore/df.csv")
+df_label = pd.read_csv("gitignore/df_label.csv")
+prediction_clip = pd.read_csv("gitignore/prediction_clip.csv")
+prediction_clip_step = pd.read_csv("gitignore/prediction_clip_step.csv")
+
+abs_score_clip_1 = 0
+abs_score_clip_step_1 = 0
+abs_score_clip_2 = 0
+abs_score_clip_step_2 = 0
+
+correct_labels_1 = df.label_niv1
+clip_labels_1 = prediction_clip.label_niv1
+clip_step_labels_1 = prediction_clip_step.label_niv1
+correct_labels_2 = df.label_niv2
+clip_labels_2 = prediction_clip.label_niv2
+clip_step_labels_2 = prediction_clip_step.label_niv2
 
 for i in range(len(df)):
-    if clip_labels[i]==correct_labels[i]:
-        abs_score_clip+=1
-    if clip_step_labels[i]==correct_labels[i]:
-        abs_score_clip_step+=1
+    if clip_labels_1[i]==correct_labels_1[i]:
+        abs_score_clip_1+=1
+    if clip_step_labels_1[i]==correct_labels_1[i]:
+        abs_score_clip_step_1+=1
+    if clip_labels_2[i]==correct_labels_2[i]:
+        abs_score_clip_2+=1
+    if clip_step_labels_2[i]==correct_labels_2[i]:
+        abs_score_clip_step_2+=1
 
-score_clip_percent = abs_score_clip/len(df)
-score_clip_step_percent = abs_score_clip_step/len(df)
+score_clip_percent_niv1 = abs_score_clip_1/len(df)
+score_clip_step_percent_niv1 = abs_score_clip_step_1/len(df)
+score_clip_percent_niv2 = abs_score_clip_2/len(df)
+score_clip_step_percent_niv2 = abs_score_clip_step_2/len(df)
 
-print("CLIP directement au niveau 2 a trouvé {} pourcent des labels".format(score_clip_percent))
-print("CLIP en mode step by step a trouvé {} pourcent des labels".format(score_clip_step_percent))
+print("CLIP directement au niveau 2 a trouvé {} pourcent des labels de niveau 1".format(score_clip_percent_niv1*100))
+print("CLIP en mode step by step a trouvé {} pourcent des labels de niveau 1".format(score_clip_step_percent_niv1*100))
+print("CLIP directement au niveau 2 a trouvé {} pourcent des labels de niveau 2".format(score_clip_percent_niv2*100))
+print("CLIP en mode step by step a trouvé {} pourcent des labels de niveau 2".format(score_clip_step_percent_niv2*100))
--- tests_clip.py
+++ tests_clip.py
@@ -1,27 +1,36 @@
-from prediction.formain import get_clip
+from formain import get_clip
 import pandas as pd
 import itertools
 import numpy as np
-import shutil
-import csv
+import os
 
-lniveau1 = ['alcoholic drink','non-alcoholic drink','baby','unprocessed poultry','butchery minced meat','butchery prepared poultry cold cuts','meat substitute', 'dessert preparation','grocery','coffe tea infusion chicory','canned grocery','frozen grocery','milk','cream','egg','cheese','creamery fat','creamery dessert','prepared fish catering', 'unprocessed fish','smoked fish','bakery','pastry','fruit vegetable','pet store','exterior','home cleaning accesory','cleaning product','human hygiene','household laundry','household appliances','craft tools','supplies','decoration','food accessories','clothing','supplies','recreation and culture','multimedia','pharmaceutical']
-lniveau2 = [['alcool','beer','wine'],['water','fruit juice','soda','flavored water','vegetable drink'],['baby food','baby milk','diapers','wipes','care','baby pharmaceutical'],['unprocessed poultry'],['minced meat'],['sausage','deli meats','prepared poultry'],['meat substitue'],['preparation for dessert'],['oil vinegar','spice condiment broth','flour','starch','pasta','rice','wheat','lentils','semolina','prepared dish','snacking','cookie','cereal','dried fruit','soup','sweets','chocolate','sauce','spreads'],['coffee','tea','infusion','chicory'],['canned vegetable','canned fruit','canned meat','canned fish'],['ice cream','french fries and similar'],['milk'],['cream'],['egg'],['cheese'],['butter','other fats'],['yoghurt','white cheese','dessert','compote'],['seafood'],['fish','shellfish'],['smoked fish'],['bread bun biscuit cake','brioche','cookies','cakes'],['pastries'],['fruits','vegetables'],['pet food','litter','hygiene','accessory'],['garden furniture','plants','insecticide','gardening tools'],['garbage can','sponge','glove','rags','tissues','paper towels','PQ','storage boxes and packaging','garbage bags','bucket','mops'],['aerosols','washing machine products','dishwasher products','detergent products','fuels'],['deodorants','cleasing gel','soap','hairbrush','cosmetics','makeup remover','hygienic protections','shaving','body hygiene','face care','dental hygiene','hair care','perfume','sex'],['household laundry'],['household appliances'],['craft tools'],['supplies'],['decoration'],['dishes','cooking paper','utensil'],['textile','leather goods'],['toys','book'],['CD','DVD','other multimedia'],['pharmaceutical']]
-l_niveau1 = []
-for i in lniveau2:
-    index = lniveau2.index(i)
+lniveau1_eng = ['alcoholic drink','non-alcoholic drink','baby','unprocessed poultry','butchery minced meat','butchery prepared poultry cold cuts','meat substitute', 'dessert preparation','grocery','coffe tea infusion chicory','canned grocery','frozen grocery','milk','cream','egg','cheese','creamery fat','creamery dessert','prepared fish catering', 'unprocessed fish','smoked fish','bakery','pastry','fruit vegetable','pet store','exterior','home cleaning accesory','cleaning product','human hygiene','household laundry','household appliances','craft tools','supplies','decoration','food accessories','clothing','supplies','recreation and culture','multimedia','pharmaceutical']
+lniveau2_eng = [['alcool','beer','wine'],['water','fruit juice','soda','flavored water','vegetable drink'],['baby food','baby milk','diapers','wipes','care','baby pharmaceutical'],['unprocessed poultry'],['minced meat'],['sausage','deli meats','prepared poultry'],['meat substitue'],['preparation for dessert'],['oil vinegar','spice condiment broth','flour','starch','pasta','rice','wheat','lentils','semolina','prepared dish','snacking','cookie','cereal','dried fruit','soup','sweets','chocolate','sauce','spreads'],['coffee','tea','infusion','chicory'],['canned vegetable','canned fruit','canned meat','canned fish'],['ice cream','french fries and similar'],['milk'],['cream'],['egg'],['cheese'],['butter','other fats'],['yoghurt','white cheese','dessert','compote'],['seafood'],['fish','shellfish'],['smoked fish'],['bread bun biscuit cake','Bakery brioche','Bakery cookies','Bakery cakes'],['pastries'],['fruits','vegetables'],['pet food','litter','hygiene','accessory'],['garden furniture','plants','insecticide','gardening tools'],['garbage can','sponge','glove','rags','tissues','paper towels','PQ','storage boxes and packaging','garbage bags','bucket','mops'],['aerosols','washing machine products','dishwasher products','detergent products','fuels'],['deodorants','cleasing gel','soap','hairbrush','cosmetics','makeup remover','hygienic protections','shaving','body hygiene','face care','dental hygiene','hair care','perfume','sex'],['household laundry'],['household appliances'],['craft tools'],['supplies'],['decoration'],['dishes','cooking paper','utensil'],['textile','leather goods'],['toys','book'],['CD','DVD','other multimedia'],['pharmaceutical']]
+l_niveau1_eng = []
+for i in lniveau2_eng:
+    index = lniveau2_eng.index(i)
     for j in range(len(i)):
-        l_niveau1.append(lniveau1[index])
-l_niveau2 = list(itertools.chain(*lniveau2))
+        l_niveau1_eng.append(lniveau1_eng[index])
+l_niveau2_eng = list(itertools.chain(*lniveau2_eng))
 
-df_label = pd.DataFrame({'niv1':l_niveau1, 'niv2':l_niveau2})
+lniveau1_fr = ['Boissons alcoolisées','Boissons sans alcool','Bébé','Volaille non prép','Boucherie - viandes hachées','Boucherie/Volaille préparé/Charcuterie','Boucherie - succédanés de viande',"Epicerie - avec mode d'emploi","Epicerie","Epicerie - sans valeurs nutritionnelles","Epicerie - Conserves (boîtes et bocaux)","Epicerie surgelée","Crémerie - laits","Crémerie - crèmes","Crémerie - oeufs","Crémerie - fromages","Crèmeries - matières grasses","Cremerie desserts","Poissonnerie traiteur /préparé","Poissonnerie non préparé","Poissonerie fumaison","Boulangerie","pâtisserie","Fruits et légumes","Animalerie","Exterieur","Entretien Maison - accessoires","Entretien produits /détergents","Hygiène humaine","Linge de maison","Electroménager","Outils de bricolage","Décoration","Accessoires alimentaires","Habits","Fourniture","Loisirs et culture","Multimedia","pharmaceutique"]
+lniveau2_fr = [['Alcools','Bieres','Vins'],['Eau','Jus de fruits','Sodas','eaux aromatises','Boissons vegetales'],['Aliments bebe','Laits bebe','Couches','Lingettes','Soin','pharmaceutique pour bebe'],['Volaille non prep'],['viandes hachees'],['Saucisses','Charcuterie','Volaille prep'],['succedanes de viande'],['Preparations pour dessert'],['Huiles et vinaigres','Epices et condiments bouillons','Farines','Fecules','Pates','Riz','Ble','Lentilles','Semoules','Plats prepares','snacking','Biscuiterie','Cereales','Fruits secs','soupes','confiseries','chocolats','sauces','tartinables'],['cafe','the','infusions','chicorees'],['Conserves de legumes','Conserves de fruits','Conserves de viandes','Conserves de poisson'],['glaces','frites et assimiles'],['Laits'],['Cremes'],['Oeufs'],['Fromages'],['Beurres','Autres matieres grasses'],['yaourts','fromages blancs','desserts','compotes'],['Traiteur mer'],['Poisson','Crustaces'],['Fumaison poisson'],['pains brioches  biscuits  gateaux','Boulangerie Brioches','Boulangerie biscuits','Boulangerie gateaux'],['patisseries'],['Fruits','Legumes'],['Nourriture pour animal','Litiere','Hygiene','Accessoire'],['Mobilier jardin','Plantes','insecticide','Outils de jardinage'],['poubelle','eponge','gant','chiffons','Mouchoirs','Essuie tout','PQ','emballage','Sacs poubelles','seaux','serpilleres'],['Aerosols','produits machine a laver','produits lave vaiselle','produits detergents','combustibles'],['Deodorants','Gel nettoyant','savon','brosse a cheveux','Cosmetiques (Maquillage)','Demaquillant','Protections hygeniques','Rasage','hygiene corporelle','Soin du visage','Hygiene dentaire','Soin des cheveux','Parfum','sexe (lubrifiant + preservatif)'],['Linge de maison'],['Electromenager'],['Outils de bricolage'],['Decoration'],['Vaisselle','Papiers cuissons','Ustensile'],['Textile','Maroquinerie'],['Fourniture'],['Jouets','Livre'],['CD','DVD','autre multimedia'],['pharmaceutique']]
+l_niveau1_fr = []
+for i in lniveau2_fr:
+    index = lniveau2_fr.index(i)
+    for j in range(len(i)):
+        l_niveau1_fr.append(lniveau1_fr[index])
+l_niveau2_fr = list(itertools.chain(*lniveau2_fr))
+
+df_label = pd.DataFrame({'niv1_eng':l_niveau1_eng, 'niv1_fr':l_niveau1_fr, 'niv2_eng':l_niveau2_eng, 'niv2_fr':l_niveau2_fr})
+df_label.to_csv("gitignore/df_label.csv")
 
 df_1 = pd.read_csv("gitignore/auchan_product_legalInfo_sample.csv", encoding='utf-8')
 df_2 = pd.read_csv("gitignore/auchan_product_media_sample.csv", encoding='utf-8')
 df = pd.merge(df_1, df_2, how='right', left_on='ATPD_00101', right_on='cui')
 df['description'] = df.ATPD_200133 + ' ' + df.product_label
 df['image_name_MEDIASTEP'] = df['photo_id']
-img_name = ['image_{}'.format(i) for i in range(1,len(df)+1)]
+img_name = ['image_{}'.format(i) for i in range(0,len(df))]
 df.insert(1, "image_name", img_name, allow_duplicates=False)
 df.insert(3, "label", 0, allow_duplicates=False)
 df = df[['image_name_MEDIASTEP','image_name', 'description', 'label', 'media_url']]
