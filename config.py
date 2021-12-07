982d299c0947a308cc31e9819ef7e70c180b637c
heez77 <74363374+heez77@users.noreply.github.com>
2021-12-07 11:30:55 +0100

    add entrainement_camemBERT

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import torch
 
 class CFG:
     path = os.getcwd()
-    path_images = '/home/jeremy/AUCHAN 2/IMAGES/DATASET V2'
+    path_data = os.path.join(path,'Data')
     path_dataframe = os.path.join(path, "df2.csv")
     path_labels = os.path.join(path, "df_label.csv")
     path_models = os.path.join(path, "Models")
-    path_bert = os.path.join(path, 'Training/CamemBERT')
+    path_bert = os.path.join(path, 'Entrainement','CamemBERT')
     threshold_clip = 0.8
     threshold_dist = 0.8
     device = "cuda" if torch.cuda.is_available() else "cpu"
