504e177caeb125cf9b5d6689de9b1ad6c209aaab
heez77 <74363374+heez77@users.noreply.github.com>
2021-12-07 13:18:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import torch
 
 class CFG:
     path = os.path.join(os.path.expanduser('~'), "Documents", "Auchan_Data")
-    path_images = os.path.join(path, "Images")
+    path_data = os.path.join(path, "Data")
     path_dataframe = os.path.join(path, "data.csv")
     path_models = os.path.join(path, "Models")
     path_bert = os.path.join(path, "Training", "CamemBERT")
@@ -12,6 +12,6 @@ class CFG:
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
 os.makedirs(CFG.path, exist_ok=True)
-os.makedirs(CFG.path_images, exist_ok=True)
+os.makedirs(CFG.path_data, exist_ok=True)
 os.makedirs(CFG.path_models, exist_ok=True)
 os.makedirs(CFG.path_bert, exist_ok=True)
